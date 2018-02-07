c918035e2688296ab6f5a5bca8ac024851f7188e
Josh Orton <jeo27@bath.ac.uk>
2018-02-07 23:57:51 +0000

    Added Population of Reasoning Arrays

--- src/js/dissertation.js
+++ src/js/dissertation.js
@@ -1,6 +1,5 @@
-
-function GenerateStatements(div, arr) {
-    for(var i = 0; i<arr.length; i++) {
+function printArrayInDiv(div, arr) {
+    for (var i = 0; i < arr.length; i++) {
         var listItem = document.createElement('li');
         var anchorItem = document.createElement('a');
         var arrayValue = arr[i];
@@ -10,39 +9,48 @@ function GenerateStatements(div, arr) {
         div.appendChild(listItem);
     }
 }
-function populateReasonsFor(div, arr){
-    Statement[i].forList, AgainstList
-    arrayFor.push(['FirstStatementReason1','FirstStatementReason2','FirstStatementReason3','FirstStatementReason4']);
-    arrayFor.push(['SecondStatementReason1','SecondStatementReason2','SecondStatementReason3','SecondStatementReason4','SecondStatementReason5']);
 
-}
 
-function populateReasonsAgainst(statementArray, againstArray) {
+function populateReasonsAgainst(statementArray, againstArray, reasoningStatementList) {
     var newList = [];
-    for(var i = 0; i < statementArray.length; i++) {
-        for(var j = 0; j< againstArray.length;j++) {
-            newList= newList.concat(arrayAgainst[j]);
-        }
-        statementArray.push(newList[i]);
+    var j = 0;
+    for (var i = 0; i < statementArray.length; i++) {
+
+        do {
+            if(againstArray[j] != statementArray[i]) {
+                console.log("AgainstArray: ", j, " ", againstArray[j]);
+                console.log("StatementArray[i+1]: ", i + 1, " ", statementArray[i + 1]);
+                console.log("StatementArray[i]: ", i, " ", statementArray[i]);
+                newList= newList.concat(againstArray[j]);
+            }
+            j = j + 1;
+        } while(((againstArray[j] != statementArray[i+1]) && j < againstArray.length));
+        //Maybe can remove length part, as when both each end of array they both equal undefined..
+        //TODO cleanup above and try to generalise for many arrays.
+        reasoningStatementList.push(newList);
+        newList = [];
     }
 }
 
 var ForListDiv = document.getElementById('ForListUL');
 var StatementListDiv = document.getElementById('StatementListUL');
 var AgainstListDiv = document.getElementById('AgainstListUL');
-var arrStatement = ['Brexit is shite', 'Immigration is horrible', 'Down with the Queen', 'There should be no encrpytion', 'Kill all kids', 'Kill all animals'];
+var statementArray = ['Brexit is shite', 'Immigration is horrible', 'Down with the Queen', 'There should be no encrpytion', 'Kill all humans', 'Kill all animals'];
+var againstStatements = [];
+var forStatements = [];
 
+var arrayFor = ['Brexit is shite', 'Agree as means we can make our own rules', 'This can be broken if statement matches argument', 'Immigration is horrible', '-1 Disagree ', 'Down with the Queen', 'Id would not kill her dogs too', 'There should be no encrpytion', 'Edward Snowden is not a traitor', 'Kill all humans', 'Very extreme', 'Kill all animals', '-1 Animals are gorgeous'];
 
-var arrayFor = [];
-var arrayAgainst = [];
+var arrayAgainst = ['Brexit is shite', 'Disagree as means we can make our own rules', 'This can be broken if statement matches argument', 'Immigration is horrible', '+ 1 agree ', 'Down with the Queen', 'Id kill her dogs too', 'There should be no encrpytion', 'Edward Snowden is a traitor', 'Kill all humans', 'Bit extreme but would save the planet', 'Kill all animals', '+1 taking up all humans space'];
 
+populateReasonsAgainst(statementArray, arrayAgainst, againstStatements);
+populateReasonsAgainst(statementArray,arrayFor, forStatements);
 
-
-var arrFor = ['Array For', 'Value 2', 'Value 3', 'Value 4', 'Value 5', 'Value 6'];
-var arrAgainst = ['Array Against', 'Value 2', 'Value 3', 'Value 4', 'Value 5', 'Value 6'];
-
-GenerateStatements(ForListDiv,arrFor);
-GenerateStatements(StatementListDiv,arrStatement);
-GenerateStatements(AgainstListDiv,arrAgainst);
-console.log(arrStatement.length);
-console.log(arrayFor[1][4]);
+printArrayInDiv(StatementListDiv, statementArray);
+printArrayInDiv(AgainstListDiv, againstStatements);
+printArrayInDiv(ForListDiv, forStatements);
+console.log(statementArray.length);
+console.log(arrayFor[4]);
+console.log(statementArray);
+console.log(againstStatements);
+console.log(arrayAgainst);
