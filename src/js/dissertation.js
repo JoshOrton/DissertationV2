8c834b0f2a8c0be492521f1c73a1c61ad0e804f1
Josh Orton <jeo27@bath.ac.uk>
2018-02-11 20:56:08 +0000

    Adding further changes to population of statements

--- src/js/dissertation.js
+++ src/js/dissertation.js
@@ -1,9 +1,10 @@
 
+//TODO CSS change ordered columns.
+var ForListDiv = document.getElementById('ForListOL');
+var StatementListDiv = document.getElementById('StatementListOL');
+var AgainstListDiv = document.getElementById('AgainstListOL');
 
-var ForListDiv = document.getElementById('ForListUL');
-var StatementListDiv = document.getElementById('StatementListUL');
-var AgainstListDiv = document.getElementById('AgainstListUL');
-
+//TODO Perhaps grab this dynamically using AJAX request to Java backend
 var statementArray = ['Brexit is shite', 'Immigration is horrible', 'Down with the Queen', 'There should be no encrpytion', 'Kill all humans', 'Kill all animals'];
 var unformattedForArguments = ['Brexit is shite', 'Agree as means we can make our own rules', 'This can be broken if statement matches argument', 'Immigration is horrible', '-1 Disagree ', 'Down with the Queen', 'Id would not kill her dogs too', 'There should be no encrpytion', 'Edward Snowden is not a traitor', 'Kill all humans', 'Very extreme', 'Kill all animals', '-1 Animals are gorgeous'];
 var unformattedAgainstArguments = ['Brexit is shite', 'Disagree as means we can make our own rules', 'This can be broken if statement matches argument', 'Immigration is horrible', '+ 1 agree ', 'Down with the Queen', 'Id kill her dogs too', 'There should be no encrpytion', 'Edward Snowden is a traitor', 'Kill all humans', 'Bit extreme but would save the planet', 'Kill all animals', '+1 taking up all humans space'];
@@ -11,27 +12,56 @@ var unformattedAgainstArguments = ['Brexit is shite', 'Disagree as means we can 
 var againstStatements = populateReasoningStatementArray(statementArray, unformattedAgainstArguments);
 var forStatements = populateReasoningStatementArray(statementArray, unformattedForArguments);
 
-printArrayInDiv(StatementListDiv, statementArray);
-printArrayInDiv(AgainstListDiv, againstStatements);
-printArrayInDiv(ForListDiv, forStatements);
+populateStatementsAsUnorderedList(StatementListDiv, statementArray, false);
+
+populateStatementsAsUnorderedList(AgainstListDiv,againstStatements,true );
+populateStatementsAsUnorderedList(ForListDiv,forStatements, true);
+
+//TODO generate statements for once clicked on statement.
+//populateStatementsAsUnorderedList(AgainstListDiv, againstStatements);
+//populateStatementsAsUnorderedList(ForListDiv, forStatements);
+
 console.log(statementArray.length);
 console.log(forStatements[4]);
 console.log(statementArray);
 console.log(againstStatements);
 
+$(StatementListDiv).click(function (event) {
+    //TODO Clear Statement, For and Against.
+    hideArrayAsUnorderedList(ForListDiv);
+    hideArrayAsUnorderedList(AgainstListDiv);
+    /*populateStatementsAsUnorderedList(AgainstListDiv,againstStatements[statementId]);
+    populateStatementsAsUnorderedList(ForListDiv,forStatements[statementId]);*/
+    var text = $(event.target).text();
+    var statementId= event.target.parentNode.id.replace("li-id-","");
+    populateStatementsAsUnorderedList(ForListDiv, forStatements[statementId]);
+    populateStatementsAsUnorderedList(AgainstListDiv, againstStatements[statementId]);
+    console.log(text);
+});
 
-function printArrayInDiv(div, arr) {
+
+// Print specified Array into specified div as unordered List.
+
+//More like populate really.
+function populateStatementsAsUnorderedList(div, arr){
     for (var i = 0; i < arr.length; i++) {
         var listItem = document.createElement('li');
         var anchorItem = document.createElement('a');
-        var arrayValue = arr[i];
-        anchorItem.setAttribute('href', "http://www.msn.com");
-        anchorItem.innerHTML = arrayValue;
+        listItem.id = 'li-id-' + i;
+        //anchorItem.setAttribute('href', "");
+        anchorItem.innerHTML = arr[i];
         listItem.appendChild(anchorItem);
         div.appendChild(listItem);
     }
 }
 
+function hideArrayAsUnorderedList(div) {
+    div.style.display = "none";
+}
+function showArrayAsUnorderedList(div) {
+    div.style.display = ""
+}
+//TODO Maybe don't need array index vs length check.
 function populateReasoningStatementArray(statementArray, unformattedArgumentArray){
     var newList = [];
     var reasoningStatementList = [];
@@ -43,8 +73,6 @@ function populateReasoningStatementArray(statementArray, unformattedArgumentArra
             }
             argumentIndex = argumentIndex + 1;
         } while((isArgumentForCurrentStatement(unformattedArgumentArray,argumentIndex,statementArray,i)) && argumentIndexInBounds(argumentIndex,unformattedArgumentArray));
-        //Maybe can remove length part, as when both each end of array they both equal undefined..
-        //TODO cleanup above and try to generalise for many arrays.
         reasoningStatementList.push(newList);
         newList = [];
     }
